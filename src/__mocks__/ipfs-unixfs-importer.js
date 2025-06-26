f6f6e4b6c439b11eccb6abd719f74f61bc1ff658
Novus Nota <68142933+novusnota@users.noreply.github.com>
2025-06-26 17:49:14 +0200

    migrate from jest to vitest

--- src/__mocks__/ipfs-unixfs-importer.js
+++
@@ -1,2 +0,0 @@
-export const importer = jest.fn();
-export default { importer };
---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    css: true,
+    reporters: [['default', { summary: false }]],
+    include: [
+      'src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+      'src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+    ],
+    coverage: {
+      include: [
+        'src/**/*.{js,jsx,ts,tsx}'
+      ],
+      exclude: [
+        'src/**/*.d.ts'
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      "^react-native$": "react-native-web",
+      '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    }
+  },
+  define: {
+    'process.env.PUBLIC_URL': '""',
+  }
+});
